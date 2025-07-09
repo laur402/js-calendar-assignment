04a9ec890c3c057c9137eb0444729dd16f683414
laur402 <laurynast@wix.com>
2025-07-09 14:51:49 +0300

    Add month constant retrieval via Intl.DateTimeFormat object, fix array contents being mutable.

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
-const THREE_LETTER_MONTHS: string[] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const THREE_LETTER_MONTHS: readonly string[] = getMonthsByLocale("short");
 const TIME_IN_A_DAY_MS: number = 24*60*60*1000; //ms in a day
-const MONTHS: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const THREE_LETTER_WEEK_DAYS: string[] = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+const MONTHS: readonly string[] = getMonthsByLocale("long");
+const THREE_LETTER_WEEK_DAYS: readonly string[] = getWeekdaysByLocale("short").map((value) => value.toUpperCase());
 const LOAD_TIME: Date = new Date();
 
 enum WeekDays {
@@ -12,4 +12,17 @@ enum WeekDays {
     "Thursday",
     "Friday",
     "Saturday"
+}
+
+type DateTimeFormatOptionsMonths = "numeric" | "2-digit" | "long" | "short" | "narrow" | undefined;
+function getMonthsByLocale(monthFormat: DateTimeFormatOptionsMonths = "numeric", locale?: string) {
+    const format = new Intl.DateTimeFormat(locale, {month: monthFormat});
+    return [...Array(12).keys()]
+        .map((value) => format.format(new Date(2000, value, 1)));
+}
+type DateTimeFormatOptionsWeekdays = "long" | "short" | "narrow" | undefined;
+function getWeekdaysByLocale(monthFormat: DateTimeFormatOptionsWeekdays, locale?: string) {
+    const format = new Intl.DateTimeFormat(locale, {weekday: monthFormat});
+    return [...Array(7).keys()]
+        .map((value) => format.format(new Date().getTime() - (new Date().getDay() - value) * TIME_IN_A_DAY_MS));
 }
